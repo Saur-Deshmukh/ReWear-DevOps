885ad9e1d331751e30830e5ae350ec56eb925848
Saur-Deshmukh <saur90044@gmail.com>
2026-04-05 19:19:15 +0530

    aws deployment done

---
+++ terraform/main.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "rewear-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0.0"
+
+  cluster_name    = "rewear-cluster"
+  cluster_version = "1.29"
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = false
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.small"]
+      desired_size   = 1
+      min_size       = 1
+      max_size       = 3
+    }
+  }
+}
